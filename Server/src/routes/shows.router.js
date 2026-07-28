4ad18e1611095cf643ceae67985b260d92d52888
Manish-code3090 <manishhimeranam@gmail.com>
2026-07-28 22:31:13 +0530

    forgeet to add

--- Server/src/routes/shows.router.js
+++ Server/src/routes/shows.router.js
@@ -1,11 +1,33 @@
 import express from "express";
-import { createShow, getAllShows, editShow, cancelShow } from "../controller/shows.controller.js";
+import {
+  createShow,
+  getAllShows,
+  editShow,
+  cancelShow,
+  getShowById,
+} from "../controller/shows.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import authorized from "../middlewares/authorized.middleware.js";
-const show_router = express.Router()
+const show_router = express.Router();
 // routes
-show_router.post('/create', authMiddleware, authorized("ADMIN", "SUPER_ADMIN", "THEATRE_OWNER"), createShow);
-show_router.get('/', authMiddleware, getAllShows);
-show_router.patch('/:id/edit', authMiddleware, authorized("ADMIN", "SUPER_ADMIN", "THEATRE_OWNER"), editShow);
-show_router.delete('/:id/cancel', authMiddleware, authorized("ADMIN", "SUPER_ADMIN", "THEATRE_OWNER"), cancelShow);
-export default show_router
+show_router.post(
+  "/create",
+  authMiddleware,
+  authorized("ADMIN", "SUPER_ADMIN", "THEATRE_OWNER"),
+  createShow,
+);
+show_router.get("/", authMiddleware, getAllShows);
+show_router.get("/:id", authMiddleware, getShowById);
+show_router.patch(
+  "/:id/edit",
+  authMiddleware,
+  authorized("ADMIN", "SUPER_ADMIN", "THEATRE_OWNER"),
+  editShow,
+);
+show_router.patch(
+  "/:id/cancel",
+  authMiddleware,
+  authorized("ADMIN", "SUPER_ADMIN", "THEATRE_OWNER"),
+  cancelShow,
+);
+export default show_router;
